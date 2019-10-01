a3627901345f68b89b423a939543e73835adad8e
drunkTolstoy <drunktolstoy@gmail.com>
2019-10-01 13:03:46 +0300

    make it better222rr

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -1,8 +1,7 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import Chat from './chat/Chat.jsx'
-import Home from './home/Home.jsx'
-
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Chat from './chat/Chat.jsx';
+import Home from './home/Home.jsx';
 
 // The Main component renders one of the three provided
 // Routes (provided that one matches). Both the /roster
@@ -10,12 +9,13 @@ import Home from './home/Home.jsx'
 // with /roster or /schedule. The / route will only match
 // when the pathname is exactly the string "/"
 const Main = () => (
-  <main>
-    <Switch>
-      <Route exact path='/' component={Home}/>
-      <Route path='/chat' component={Chat}/>
-    </Switch>
-  </main>
-)
+	<main>
+		<Switch>
+			<Route exact path="/" component={Home} />
+			<Route path="/chat" component={Chat} />
+			<Route component={Chat} />
+		</Switch>
+	</main>
+);
 
-export default Main
+export default Main;
