fdb655b03db39f11a454c33d6d23d20e13829c9a
drunkTolstoy <drunktolstoy@gmail.com>
2020-08-01 17:06:18 +0300

    fixed prod src prombles

--- server/server.js
+++ server/server.js
@@ -112,12 +112,12 @@ exports.EmitToRoom = function(room, eventName, msg) {
 //exports.EmitToSocket = function(socket, msg) {};
 if (process.env.NODE_ENV === 'production') {
 	// Serve any static files
-	app.use(express.static(path.join(__dirname, 'client/build'))).use(cors()).use(cookieParser());
+	app.use(express.static(path.join(__dirname, '../client/build'))).use(cors()).use(cookieParser());
 	app.use(express.static('client/build'));
 
 	// Express serve up index.html file if it doesn't recognize route
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+		res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
 	});
 }
 function generateRandomString(length) {
